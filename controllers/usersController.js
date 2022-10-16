e437e190162ce48696e48dadd8b5a84d48cb35f2
DavidJoao <davidsandoval596@gmail.com>
2022-10-16 11:28:16 -0700

    changed login request

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,8 +3,12 @@ const app = express()
 const router = express.Router();
 const User = require('../schemas/user')
 const Pueblo = require('../schemas/pueblo')
-const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt');
+const { JsonWebTokenError } = require('jsonwebtoken');
 // const { requireToken, createUserToken } = require('../middleware/auth')
+
+tokenSecret = process.env.ACCESS_TOKEN_SECRET
 
 //user crud
 // index
@@ -14,4 +18,13 @@ router.get('/', (req, res) => {
     res.json(users)
 })
 
+
+//login user
+app.post('/login', (req, res) => {
+    const useranme = req.body.username
+    const user = { name: username }
+
+    const accessToken = jwt.sign(user, tokenSecret)
+    res.json({ accessToken })
+})
 module.exports = router
