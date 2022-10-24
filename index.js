ddcc898078559ae1038650be7b34e72ff4eb7b59
DavidJoao <davidsandoval596@gmail.com>
2022-10-23 17:53:15 -0700

    created token function for login, working on the creation of a side server authentication

--- index.js
+++ index.js
@@ -10,17 +10,40 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//authentication
+const jwt = require('jsonwebtoken')
+const tokenSecret = process.env.ACCESS_TOKEN_SECRET;
+
 //routes // redirect
 
 app.get('/', (req, res) => {
     res.redirect('/pueblos')
 });
-//controllers
-const usersControllers = require('./controllers/usersController');
-app.use('/users', usersControllers);
 
-const pueblosControllers = require('./controllers/pueblosController')
-app.use('/pueblos', pueblosControllers);
+const pueblos = [
+    {
+        name: 'Nochistlan',
+        state: 'Zacatecas'
+    },
+    {
+        name: 'Jerez',
+        state: 'Zacatecas'
+    }
+]
+
+app.get('/pueblos', authenticateToken, (req, res) => {
+    res.json(pueblos)
+})
+
+app.post('/login', (req, res) => {
+    //User authentication
+    const username = req.body.username
+    const user = { name: username }
+    
+    const accessToken = jwt.sign(user, tokenSecret)
+    res.json({ accessToken: accessToken })
+
+})
 
 //error handling
 app.use((err, req, res, next) => {
@@ -28,6 +51,20 @@ app.use((err, req, res, next) => {
     const message = err.message || 'Internal Server Error'
     res.status(statusCode).send(message);
 });
+
+
+//authentication function
+function authenticateToken(req, res, next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if(token == null) return res.sendStatus(401)
+
+    jwt.verify(token, tokenSecret, (err, user) => {
+        if(err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
 
 //start server
 
